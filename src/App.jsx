b7f49f5bc5326b52dfadd9255adc709d1773db82
jvmichelotti <jvbulegon@gmail.com>
2024-10-10 20:06:31 -0300

    Fazendo Deploy

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function Pagina() {
+
+  return <main>
+
+    <img src="./myself.jpg" alt="foto" width="128"/>
+
+    <div>
+      <b>João Vitor Michelotti</b> | 24 anos | Desenvolverdor Web
+    </div>
+
+    <div>
+      <b>Perfil</b> : Olá eu sou o João e sou Desenvolvedor Web.
+      Adoro programar em React e em outras diversas tecnologias novas.
+    </div>
+
+    <div>
+      <b>Experiências</b> : Já trabalhei com PHP,
+      desenvolvendo projetos como EsportsLearner
+      e passei 3 anos fazendo o curso na SuperGeeks!.
+    </div>
+
+    <div>
+      <b>Competências</b> : 
+      Lógica de programação,
+      Raciocínio computacional,
+      Python,
+      C#,
+      HTML,
+      CSS,
+      JavaScript e 
+      React.
+    </div>
+
+    <div>
+      <a href="#">Instagram</a>
+      <a href="#">Linkedin</a>
+      <a href="https://github.com/JoaoMichelotti">GitHub</a>
+    </div>
+
+  </main>
+}
